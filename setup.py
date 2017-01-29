b1453e2bc5bc93fb9b9b34be0cbf8bb605974517
Krypticdator <toni.nurmi@hotmail.com>
2017-01-29 18:13:03 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='FSTTRPGLifepath',
+    version='0.0.1',
+    packages=['fsttrpglifepath'],
+    url='',
+    license='',
+    author='Toni Nurmi',
+    author_email='',
+    description=''
+)
